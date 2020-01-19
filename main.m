65ba7f2211c07090ad6a45877e33ce4287c1fc4c
martiabr <martin.abrandt@outlook.com>
2020-01-19 13:19:22 +0100

    Plots

--- main.m
+++ main.m
@@ -4,10 +4,10 @@
 % Possibly try more advanced model after? Terrain?
 
 % Constants:
-g = 9.81;
-r = 1;
-m = 1;
-l_0 = 1;
+g = 9.81;  % gravity
+r = 1;  % spring constant
+m = 1;  % mass
+l_0 = 1;  % equilibrium leg length
 
 % Let y be horizontal distance, z be vertical distance, w be angular
 % velocity of body, theta be counter-clockwise leg angle from y axis and l
@@ -41,27 +41,75 @@ to = @(x_s, u) cos(x_s(1)) * x_s(2) * x_s(3) + sin(x_s(1)) * x_s(4) > 0 && ...
 dt = 0.001;
 K = 1000;
 t = 0:dt:((K - 1) * dt);
+s = zeros(K);  % state variable, 0 is flight and 1 is stance
+s(1) = 1;
 n_f = 5;
 n_s = 4;
 x_f = zeros(n_f, K);
-x_f(:, 1) = [0 0.01 1 0 0]';
-s = 0;  % state variable, 0 is flight and 1 is stance
+x_f(:, 1) = [0 0.1 2 0 pi/2]';
+x_s = zeros(n_s, K);
+x_s(:, 1) = [0 0 l_0 0]';
+u = [0 0]';
 
 % Simulation:
 for k = 1:(K - 1)
-    if s
+    if s(k)
         % Simulate stance dynamics:
+        x_s(:, k + 1) = x_s(:, k) + f_s(x_s(:, k), u) * dt;
+        
+        if to(x_s(:, k + 1))
+            s(k + 1) = 0;
+            % Do coord transformation here
+        else
+            s(k + 1) = 1;
+        end
     else
         % Simulate flight dynamics:
-        x_f(:, k + 1) = x_f(:, k) + f_f(x_f(:, k), 0.1) * dt;
-    end
-    if td(x_f(:, k + 1))
-        break
+        x_f(:, k + 1) = x_f(:, k) + f_f(x_f(:, k), 1) * dt;
+        
+        if td(x_f(:, k + 1))
+            s(k + 1) = 1;
+            % Do coord transformation here
+        else
+            s(k + 1) = 0;
+        end
     end
 end
 
 % Plot:
 figure(1); clf;
 plot(x_f(1, :), x_f(3, :));
+%plot(t, x_f(3, :) - l_0 * sin(x_f(5, :)));
+
+figure(2); clf;
+subplot(3, 2, 1);
+plot(t, x_f(1, :));
+title('y');
+subplot(3, 2, 2);
+plot(t, x_f(2, :));
+title('y dot');
+subplot(3, 2, 3);
+plot(t, x_f(3, :));
+title('z');
+subplot(3, 2, 4);
+plot(t, x_f(4, :));
+title('z dot');
+subplot(3, 2, 5);
+plot(t, x_f(5, :));
+title('theta');
+
+figure(3); clf;
+subplot(2, 2, 1);
+plot(t, x_s(1, :));
+title('theta');
+subplot(2, 2, 2);
+plot(t, x_s(2, :));
+title('theta dot');
+subplot(2, 2, 3);
+plot(t, x_s(3, :));
+title('l');
+subplot(2, 2, 4);
+plot(t, x_s(4, :));
+title('l dot');
 
 
