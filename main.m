c8996fdeea71f85b4ce812e979162ddef6f45158
martiabr <martin.abrandt@outlook.com>
2020-01-20 21:16:10 +0100

    Add basic animation. Still some bug when switching state

--- main.m
+++ main.m
@@ -55,32 +55,37 @@ s2f = @(x_s, y_f) [cos(x_s(1)) * x_s(3) + y_f;
 
 % Simulation initialization:
 dt = 0.0001;
-K = 4000;
+K = 20000;
 t = 0:dt:((K - 1) * dt);
 s = zeros(1, K);  % state variable, 0 is flight and 1 is stance
-s(1) = 1;
+s(1) = 0;
 n_f = 5;
 n_s = 4;
 x_f = zeros(n_f, K);
-x_f(:, 1) = [0 0.1 2 0 1.6]';
-y_f = 0;
+x_f(:, 1) = [l_0 * cos(1.55) 0 2 0 1.55]';
+y_f = zeros(1, K);
 x_s = zeros(n_s, K);
-x_s(:, 1) = [1.6 -0.8 0.8 -0.8]';
+%x_s(:, 1) = [1.6 -0.4 0.8 -0.8]';
 u = [0 0]';
-w = -0.3;
+w = 0;
 
 % Simulation:
 for k = 1:(K - 1)
+    if x_f(3, k) < 0
+        break
+    end
     if s(k)
         % Simulate stance dynamics:
         x_s(:, k + 1) = x_s(:, k) + f_s(x_s(:, k), u) * dt;
         
+        y_f(k + 1) = y_f(1, k);
+        
+        x_f(:, k + 1) = s2f(x_s(:, k + 1), y_f(k + 1));
+       
         if k > 1 && to(x_s(:, k + 1), x_s(:, k), u)  % detect takeoff
             s(k + 1) = 0;
             w = x_s(2, k + 1);  % set new angular velocity
-            x_f(:, k + 1) = s2f(x_s(:, k + 1), y_f);
-            x_s(:, k + 1)
-            x_f(:, k + 1)
+            x_f(:, k + 1) = s2f(x_s(:, k + 1), y_f(k + 1));
         else
             s(k + 1) = 1;
         end
@@ -88,12 +93,15 @@ for k = 1:(K - 1)
         % Simulate flight dynamics:
         x_f(:, k + 1) = x_f(:, k) + f_f(x_f(:, k), w) * dt;
         
+        y_f(k + 1) = x_f(1, k + 1) - cos(x_f(5, k + 1)) * l_0; % WRONG POSSIBLY set new stance position
+        
+        x_s(:, k + 1) = f2s(x_f(:, k + 1), y_f(k + 1));
+        x_s(3, k + 1) = l_0;
+        x_s(4, k + 1) = 0;
+        
         if k > 1 && td(x_f(:, k + 1), x_f(:, k))  % detect touchdown
             s(k + 1) = 1;
-            y_f = x_f(1, k + 1); % set new stance position
-            x_s(:, k + 1) = f2s(x_f(:, k + 1), y_f);
-            x_s(:, k + 1)
-            x_f(:, k + 1)
+            x_s(:, k + 1) = f2s(x_f(:, k + 1), y_f(k + 1));
         else
             s(k + 1) = 0;
         end
@@ -110,33 +118,28 @@ plot(t, cos(x_s(1, :)) .* x_s(2, :) .* x_s(3, :) + sin(x_s(1, :)) .* x_s(4, :));
 
 
 figure(2); clf;
-subplot(3, 2, 1);
+subplot(2, 4, 1);
 plot(t, x_f(1, :));
 title('y');
-subplot(3, 2, 2);
+subplot(2, 4, 2);
 plot(t, x_f(2, :));
 title('y dot');
-subplot(3, 2, 3);
+subplot(2, 4, 3);
 plot(t, x_f(3, :));
 title('z');
-subplot(3, 2, 4);
+subplot(2, 4, 4);
 plot(t, x_f(4, :));
 title('z dot');
-subplot(3, 2, 5);
+subplot(2, 4, 5);
 plot(t, x_f(5, :));
 title('theta');
-
-figure(3); clf;
-subplot(2, 2, 1);
-plot(t, x_s(1, :));
-title('theta');
-subplot(2, 2, 2);
+subplot(2, 4, 6);
 plot(t, x_s(2, :));
 title('theta dot');
-subplot(2, 2, 3);
+subplot(2, 4, 7);
 plot(t, x_s(3, :));
 title('l');
-subplot(2, 2, 4);
+subplot(2, 4, 8);
 plot(t, x_s(4, :));
 title('l dot');
 
@@ -146,3 +149,40 @@ plot(t, s);
 figure(5); clf;
 plot(x_f(1, :), x_f(3, :));
 
+figure(6); clf;
+plot(t, x_f(3, :) - l_0 * sin(x_f(5, :)));
+hold on;
+grid on;
+plot(t, x_f(4, :));
+
+figure(7); clf;
+plot(t, y_f);
+
+% Animation:
+style1 = hgexport('factorystyle');
+style1.Format = 'png';
+style1.Width = 6;
+style1.Height = 2.5;
+style1.Resolution = 300;
+style1.Units = 'inch';
+style1.FixedFontSize = 12;
+
+c1 = [66, 80, 207] / 256;
+c2 = [201, 40, 51] / 256;
+c3 = [168, 56, 201] / 256;
+
+fig = figure(8); clf;
+set(gcf,'color','w');
+
+for k = 1:50:K
+    plot(x_f(1, k), x_f(3, k), 'Marker', 'o', 'Color', c2, 'MarkerFaceColor', c2);
+    grid on
+    hold on
+    plot(x_f(1, k) - x_s(3, k) * cos(x_f(5, k)), x_f(3, k) - x_s(3, k) * sin(x_f(5, k)), 'Marker', 'o', 'Color', c2, 'MarkerFaceColor', c2);
+    
+    axis equal
+    axis([-0.5, 2.5, -0.5, 2.5]);
+    
+    drawnow
+    hold off
+end
