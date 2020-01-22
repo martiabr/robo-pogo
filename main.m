b660e78382abb22202f8480ae2a778d90ac9ffe9
martiabr <martin.abrandt@outlook.com>
2020-01-22 14:28:07 +0100

    Fix bugs. Simulation runs correctly now

--- main.m
+++ main.m
@@ -41,8 +41,8 @@ to = @(x_s, x_so, u) cos(x_s(1)) * x_s(2) * x_s(3) + sin(x_s(1)) * x_s(4) > 0 &&
                      cos(x_so(1)) * u(2) / (m * x_so(3))));
            
 % Flight to stance coordinate transformation (y_f is stance position):
-f2s = @(x_f, y_f) [acot(x_f(1)/x_f(3));
-              (x_f(1) * x_f(4) - x_f(3) * x_f(2)) / ((x_f(1) - y_f)^2 + x_f(3)^2);
+f2s = @(x_f, y_f) [x_f(5);
+              (x_f(1) * x_f(4) - x_f(2) * x_f(3)) / ((x_f(1) - y_f)^2 + x_f(3)^2);
               sqrt((x_f(1) - y_f)^2 + x_f(3)^2);
               (x_f(1) * x_f(2) + x_f(3) * x_f(4)) / sqrt((x_f(1) - y_f)^2 + x_f(3)^2)];
 
@@ -51,11 +51,11 @@ s2f = @(x_s, y_f) [cos(x_s(1)) * x_s(3) + y_f;
              -sin(x_s(1)) * x_s(2) * x_s(3) + cos(x_s(1)) * x_s(4);
               sin(x_s(1)) * x_s(3);
               cos(x_s(1)) * x_s(2) * x_s(3) + sin(x_s(1)) * x_s(4);
-              x_s(1)];
+              x_s(1)]; 
 
 % Simulation initialization:
 dt = 0.0001;
-K = 20000;
+K = 50000;
 t = 0:dt:((K - 1) * dt);
 s = zeros(1, K);  % state variable, 0 is flight and 1 is stance
 s(1) = 0;
@@ -65,8 +65,8 @@ x_f = zeros(n_f, K);
 x_f(:, 1) = [l_0 * cos(1.55) 0 2 0 1.55]';
 y_f = zeros(1, K);
 x_s = zeros(n_s, K);
-%x_s(:, 1) = [1.6 -0.4 0.8 -0.8]';
-u = [0 0]';
+%x_s(:, 1) = [1.57 -0.2 0.6 0]';
+u = [0 1.5]';
 w = 0;
 
 % Simulation:
@@ -78,14 +78,13 @@ for k = 1:(K - 1)
         % Simulate stance dynamics:
         x_s(:, k + 1) = x_s(:, k) + f_s(x_s(:, k), u) * dt;
         
-        y_f(k + 1) = y_f(1, k);
+        y_f(k + 1) = y_f(k);
         
         x_f(:, k + 1) = s2f(x_s(:, k + 1), y_f(k + 1));
        
         if k > 1 && to(x_s(:, k + 1), x_s(:, k), u)  % detect takeoff
             s(k + 1) = 0;
             w = x_s(2, k + 1);  % set new angular velocity
-            x_f(:, k + 1) = s2f(x_s(:, k + 1), y_f(k + 1));
         else
             s(k + 1) = 1;
         end
@@ -93,15 +92,14 @@ for k = 1:(K - 1)
         % Simulate flight dynamics:
         x_f(:, k + 1) = x_f(:, k) + f_f(x_f(:, k), w) * dt;
         
-        y_f(k + 1) = x_f(1, k + 1) - cos(x_f(5, k + 1)) * l_0; % WRONG POSSIBLY set new stance position
+        y_f(k + 1) = x_f(1, k + 1) - cos(x_f(5, k + 1)) * l_0; % set new stance position
         
         x_s(:, k + 1) = f2s(x_f(:, k + 1), y_f(k + 1));
+        x_s(2, k + 1) = w;
         x_s(3, k + 1) = l_0;
-        x_s(4, k + 1) = 0;
         
         if k > 1 && td(x_f(:, k + 1), x_f(:, k))  % detect touchdown
             s(k + 1) = 1;
-            x_s(:, k + 1) = f2s(x_f(:, k + 1), y_f(k + 1));
         else
             s(k + 1) = 0;
         end
@@ -115,7 +113,6 @@ plot(t, r / m * sin(x_s(1, :)) .* (l_0 - x_s(3, :) + u(1)) + ...
 hold on;
 grid on;
 plot(t, cos(x_s(1, :)) .* x_s(2, :) .* x_s(3, :) + sin(x_s(1, :)) .* x_s(4, :));  % vertical velocity > 0
-
 
 figure(2); clf;
 subplot(2, 4, 1);
@@ -157,6 +154,7 @@ plot(t, x_f(4, :));
 
 figure(7); clf;
 plot(t, y_f);
+title('y_f');
 
 % Animation:
 style1 = hgexport('factorystyle');
@@ -174,12 +172,15 @@ c3 = [168, 56, 201] / 256;
 fig = figure(8); clf;
 set(gcf,'color','w');
 
-for k = 1:50:K
-    plot(x_f(1, k), x_f(3, k), 'Marker', 'o', 'Color', c2, 'MarkerFaceColor', c2);
+for k = 1:250:K
+    mass_pos = [x_f(1, k); x_f(3, k)];
+    rod_end_pos = [x_f(1, k) - x_s(3, k) * cos(x_f(5, k)) x_f(3, k) - x_s(3, k) * sin(x_f(5, k))];
+   
+    plot([mass_pos(1) rod_end_pos(1)], [mass_pos(2) rod_end_pos(2)], 'LineStyle', '-', 'LineWidth', 3, 'Color', c3);
     grid on
     hold on
-    plot(x_f(1, k) - x_s(3, k) * cos(x_f(5, k)), x_f(3, k) - x_s(3, k) * sin(x_f(5, k)), 'Marker', 'o', 'Color', c2, 'MarkerFaceColor', c2);
-    
+    plot(mass_pos(1), mass_pos(2), 'Marker', 'o', 'Color', c2, 'MarkerFaceColor', c2);
+    plot(rod_end_pos(1), rod_end_pos(2), 'Marker', 'o', 'Color', c2, 'MarkerFaceColor', c2);
     axis equal
     axis([-0.5, 2.5, -0.5, 2.5]);
     
